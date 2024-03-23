0385b622d20167ac28e3479511f9dd5e51043366
Chinmay6824 <130574676+Chinmay6824@users.noreply.github.com>
2024-03-23 08:21:00 +0530

    Add files via upload

---
+++ bubbles.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int binary_search(int arr[], int n, int target) {
+    int left = 0;
+    int right = n - 1;
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == target) {
+            return mid;
+        }
+        if (arr[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return -1;
+}
+
+int main() {
+    int arr[] = {1, 3, 5, 7, 9};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int target = 5;
+    int index = binary_search(arr, n, target);
+    if (index != -1) {
+        printf("Element %d found at index %d.\n", target, index);
+    } else {
+        printf("Element %d not found in the array.\n", target);
+    }
+    return 0;
+}
